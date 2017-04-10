b7c1c4490c3d404d67cf77a9e598e6872a8edda2
Uice Lu <uicestone@gmail.com>
2017-04-10 17:49:46 +0800

    优化传感器查询存储速度

--- server/models/bean.js
+++ server/models/bean.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const beanSchema = new Schema({
+	client: {_id: Schema.ObjectId, name: String},
     mac: String,
     temp: Number,
     humi: Number,
     distance: Number,
     lastUpdatedAt: Date,
-    records: [{temp: Number, humi: Number, distance: Number, updatedAt: Date}]
+    records: {type: Array, select: false, schema: [{temp: Number, humi: Number, distance: Number, updatedAt: Date}]}
 });
 
 beanSchema.index({mac:1}, {unique:true});
